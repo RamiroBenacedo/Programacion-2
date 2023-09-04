6a21097fe87bcf7e2a85594de40adaf7c0e1d42b
JoacoTitievsky <jtitievsky@gmail.com>
2023-09-04 12:34:49 -0300

    segunda tabla db

--- db/data.sql
+++ db/data.sql
@@ -5,7 +5,7 @@ USE usuarios;
 CREATE TABLE usuario (
 
 /* nombreDeColumna  tipoDeDato  restricciones */
-id_usuario       INT         UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+idUsuario       INT         UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 pass             VARCHAR(50) NOT NULL,
 email            VARCHAR(50) NOT NULL,
 fecha            INT         NOT NULL,
@@ -14,3 +14,15 @@ dni              INT         NOT NULL,
 createdAt                    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 updatedAt                    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 deletedAt                    TIMESTAMP DEFAULT CURRENT_TIMESTAMP );
+
+CREATE TABLE posteosUsuario (
+/* nombreDeColumna  tipoDeDato  restricciones */
+idUsuario       		INT         	UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+clienteId				INT UNSIGNED,
+campoTextoNombreImg		VARCHAR(200)	NOT NULL,
+campoTextoPiePost		VARCHAR(200)	NOT NULL,
+createdAt               TIMESTAMP 		DEFAULT CURRENT_TIMESTAMP,
+updatedAt               TIMESTAMP 		DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+deletedAt               TIMESTAMP 		NULL,
+FOREIGN KEY (clienteId) REFERENCES usuario (idUsuario)
+);
