ff5e223b00b6f351a26bca923d19418fba7de4b3
JoacoTitievsky <jtitievsky@gmail.com>
2023-09-08 08:36:50 -0300

    modifique index.ejs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const data = require("../db/data")
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', )
+
+router.get('/miperfil/id/:id', )
+
+router.get('/miperfil/login/id/:id', )
+
+router.get('/miperfil/registracion/id/:id', )
+
+router.get('/miperfil/editarperfil/id/:id', )
+
+router.get('/resultadobusqueda/id/:id', )
+
+router.get('/detallepost/id/:id', )
+
+router.get('/detalleusuario/id/:id', )
+
+router.get('/agregarpost/id/:id', )
+
 
 module.exports = router;
