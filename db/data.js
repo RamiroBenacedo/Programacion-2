d2c9dd5eb513c603bdffde6c2ab484902de29e00
JoacoTitievsky <jtitievsky@gmail.com>
2023-09-09 17:23:35 -0300

    agregando datos a data.js

--- db/data.js
+++ db/data.js
@@ -1,93 +1,93 @@
 const data = {
     usuarios : [
         {
-            id: 1
-            email:'pablo@gmail.com'
-            clave:'pablo'
-            fotoPerfil: ''
-            fecha:
-            dni:
+            id: 1,
+            email:'pablo@gmail.com',
+            clave:'pablo',
+            fotoPerfil: '',
+            fecha: '15/12/1865',
+            dni: '44986213'
         },
 
         {
-            id:
-            email:
-            clave:
-            fotoPerfil
-            fecha:
-            dni:
+            id: 2,
+            email: 'maria@gmail.com',
+            clave: 'maria',
+            fotoPerfil: ,
+            fecha: '20/09/2002' ,
+            dni: '42871032'
         },
 
         {
-            id:
-            email:
-            clave:
-            fotoPerfil
-            fecha:
-            dni:
+            id: 3,
+            email: 'martin@gmail.com',
+            clave: 'martin',
+            fotoPerfil: ,
+            fecha: '16/07/1924',
+            dni: '78951468'
         },
 
         {
-            id:
-            email:
-            clave:
-            fotoPerfil
-            fecha:
-            dni:
+            id: 4,
+            email:'paula@gmail.com',
+            clave: 'paula',
+            fotoPerfil: ,
+            fecha: '28/10/1970',
+            dni: '45236987'
         },
 
         {
-            id:
-            email:
-            clave:
-            fotoPerfil
-            fecha:
-            dni:
+            id: 5,
+            email: 'franco@gmail.com',
+            clave: 'franco',
+            fotoPerfil: ,
+            fecha:'03/03/2003',
+            dni: '46257465'
         },
 
         {
-            id:
-            email:
-            clave:
-            fotoPerfil
-            fecha:
-            dni:
+            id: 6,
+            email: 'lola@gmail.com',
+            clave: 'lola',
+            fotoPerfil: ,
+            fecha: '30/12/1999',
+            dni: '40458965'
         },
 
         {
-            id:
-            email:
-            clave:
-            fotoPerfil
-            fecha:
-            dni:
+            id: 7,
+            email: 'tomas@gmail.com',
+            clave: 'tomas',
+            fotoPerfil: ,
+            fecha: '19/10/2005',
+            dni: '48987201'
         },
 
         {
-            id:
-            email:
-            clave:
-            fotoPerfil
-            fecha:
-            dni:
+            id: 8
+            email: 'magui@gmail.com',
+            clave: 'magui',
+            fotoPerfil: ,
+            fecha: '20/12/1975',
+            dni: '22659874'
         },
 
         {
-            id:
-            email:
-            clave:
-            fotoPerfil
-            fecha:
-            dni:
+            id: 9,
+            email: 'raul@gmail.com',
+            clave: 'raul',
+            fotoPerfil: ,
+            fecha: '08/11/2001',
+            dni: '43598712'
         },
 
         {
-            id:
-            email:
-            clave:
-            fotoPerfil
-            fecha:
-            dni:
+            id: 10
+            email: 'emma@gmail.com',
+            clave: 'emma',
+            fotoPerfil: ,
+            fecha: '18/08/1980',
+            dni: '38965417'
         },
     ]
 
