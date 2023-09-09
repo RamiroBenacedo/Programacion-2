61d0ad5ca928aca9e1cf1a4963cdf4383da0c02d
JoacoTitievsky <jtitievsky@gmail.com>
2023-09-09 11:09:54 -0300

    creando data.js

---
+++ db/data.js
@@ -0,0 +1,94 @@
+const data = {
+    usuarios : [
+        {
+            id: 1
+            email:'pablo@gmail.com'
+            clave:'pablo'
+            fotoPerfil: ''
+            fecha:
+            dni:
+        },
+
+        {
+            id:
+            email:
+            clave:
+            fotoPerfil
+            fecha:
+            dni:
+        },
+
+        {
+            id:
+            email:
+            clave:
+            fotoPerfil
+            fecha:
+            dni:
+        },
+
+        {
+            id:
+            email:
+            clave:
+            fotoPerfil
+            fecha:
+            dni:
+        },
+
+        {
+            id:
+            email:
+            clave:
+            fotoPerfil
+            fecha:
+            dni:
+        },
+
+        {
+            id:
+            email:
+            clave:
+            fotoPerfil
+            fecha:
+            dni:
+        },
+
+        {
+            id:
+            email:
+            clave:
+            fotoPerfil
+            fecha:
+            dni:
+        },
+
+        {
+            id:
+            email:
+            clave:
+            fotoPerfil
+            fecha:
+            dni:
+        },
+
+        {
+            id:
+            email:
+            clave:
+            fotoPerfil
+            fecha:
+            dni:
+        },
+
+        {
+            id:
+            email:
+            clave:
+            fotoPerfil
+            fecha:
+            dni:
+        },
+    ]
+
+}
