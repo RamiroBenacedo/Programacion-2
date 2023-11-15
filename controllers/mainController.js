2284f2db8106a9aac4484d47aa9151cfcc876824
JoacoTitievsky <jtitievsky@gmail.com>
2023-11-15 09:44:29 -0300

    Arreglando relaciones

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -11,8 +11,8 @@ const mainController = {
       order: [['createdAt', 'DESC']]
     })
     .then((data) => {
+      res.send(data)
        res.render('index', {posteos: data})
-       return res.send(data)
     })
     .catch((error) =>{
       return res.send({data:error});
